70441df5e9a095992091d6071f7372c9318e2b6b
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-06-21 17:13:01 -0400

    Setup example for assembly line

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//"Hello, World!"
+
+
+TheFactory BuildTheFactory() {
+    TheFactory ourNewFactory = new TheFactory();
+    return ourNewFactory;
+}
+
+TheFactory newFactory = BuildTheFactory();
+newFactory.StartAssemblyLine();
+
+class TheFactory {
+    void SaysMessageToUser (string msg, int numberOfMessages) {
+        Console.WriteLine(msg);
+        Console.WriteLine(numberOfMessages.ToString());
+    }
+
+    string GetMessage () {
+        string messageToUser = "Hello, World";
+        return messageToUser;
+    }
+
+    void SupervisorGivesOrders () {
+        string theMessage = GetMessage();
+        SaysMessageToUser(theMessage, 1);
+    }
+
+    public void StartAssemblyLine() {
+        Console.WriteLine("Workers!!!! Line up on the Wall!");
+        Console.WriteLine("Assembly Line Starts");
+        SupervisorGivesOrders();
+    }
+}
